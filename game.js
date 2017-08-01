18540848fa8095d913802a9da6aa35e73ca3c6c0
michele.tansu <michele.tansu@inpeco.com>
2017-08-01 17:55:45 +0200

    Add bullet and shots

--- game.js
+++ game.js
@@ -2,7 +2,12 @@
   const bump = new Bump(PIXI);
   const Sprite = PIXI.Sprite;
   const loader = PIXI.loader;
-  const velocity = 2;
+  const Texture = PIXI.Texture;
+  const velocity = 2,
+    bulletSpeed = 5,
+    numberOfAliens = 6,
+    spacing = 100,
+    xOffset = 150;
 
   let stage = new PIXI.Container();
   stage.height = 500;
@@ -12,14 +17,15 @@
   renderer.backgroundColor = 0x897A20;
   document.body.appendChild(renderer.view);
 
-  let aFace, aliens = [];
+  let aFace, aliens = [], bullets = [];
   let message, state;
+
+  let bulletTex = Texture.fromImage('images/bullet.png');
 
   loader.add("images/tileset.json")
     .load(setup);
 
-  function setup() {
-
+  function setupMessage() {
     message = new PIXI.Text(
       "Hello Pixi!",
       {fontFamily: "Arial", fontSize: 32, fill: "white"}
@@ -27,13 +33,9 @@
 
     message.position.set(54, 96);
     stage.addChild(message);
+  }
 
-    const id = loader.resources["images/tileset.json"].textures;
-
-    let numberOfAliens = 6,
-        spacing = 100,
-        xOffset = 150;
-
+  function setupAliens(id) {
     for (let i = 0; i < numberOfAliens; i++) {
       let alien = new Sprite(id["alien.png"]);
       alien.circular = true;
@@ -42,18 +44,46 @@
       stage.addChild(alien);
       aliens.push(alien);
     }
+  }
 
+  function setupFace(id) {
     aFace = new Sprite(id["face.png"]);
     aFace.circular = true;
+    // center the sprite's anchor point
+    aFace.anchor.x = 0.5;
+    aFace.anchor.y = 0.5;
+
     aFace.x = 1000 - 100;
     aFace.y = 500 / 2 - aFace.height / 2;
     aFace.vx = 0;
     aFace.vy = 0;
 
     stage.addChild(aFace);
+  }
+
+  function setup() {
+
+    const id = loader.resources["images/tileset.json"].textures;
+
+    setupMessage();
+
+    setupAliens(id);
+
+    setupFace(id);
+
+    stage.interactive = true;
+
+    stage.on("mousedown", function(e){
+      shoot(aFace.rotation, {
+        x: aFace.position.x+Math.cos(aFace.rotation)*20,
+        y: aFace.position.y+Math.sin(aFace.rotation)*20
+      });
+    });
+
+    animate();
 
     //Capture the keyboard arrow keys
-    keyboardSetup(aFace);
+    setupKeyboard(aFace);
 
     state = play;
 
@@ -68,11 +98,52 @@
   }
 
   function play() {
+    collisionsHandler();
+    aFace.x += aFace.vx;
+    aFace.y += aFace.vy;
+  }
+
+  function shoot(rotation, startPosition){
+    let bullet = new Sprite(bulletTex);
+    bullet.position.x = startPosition.x;
+    bullet.position.y = startPosition.y;
+    bullet.rotation = rotation;
+    stage.addChild(bullet);
+    bullets.push(bullet);
+  }
+
+  function rotateToPoint(mx, my, px, py){
+    let dist_Y = my - py;
+    let dist_X = mx - px;
+    let angle = Math.atan2(dist_Y,dist_X);
+    //var degrees = angle * 180/ Math.PI;
+    return angle;
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    // just for fun, let's rotate mr rabbit a little
+    aFace.rotation = rotateToPoint(
+      renderer.plugins.interaction.mouse.global.x,
+      renderer.plugins.interaction.mouse.global.y,
+      aFace.position.x,
+      aFace.position.y);
+
+    for (let b = bullets.length - 1; b >= 0; b--) {
+      bullets[b].position.x += Math.cos(bullets[b].rotation) * bulletSpeed;
+      bullets[b].position.y += Math.sin(bullets[b].rotation) * bulletSpeed;
+    }
+    // render the container
+    renderer.render(stage);
+  }
+
+  function collisionsHandler() {
     let collision = false;
-    for (let i=0; i<aliens.length; i++) {
+    for (let i = 0; i < aliens.length; i++) {
       if (bump.hit(aFace, aliens[i])) {
         collision = true;
-        break; 
+        break;
       }
     }
     if (collision) {
@@ -84,8 +155,6 @@
       //text and the box's color
       message.text = "No collision...";
     }
-    aFace.x += aFace.vx;
-    aFace.y += aFace.vy;
   }
 
   function randomInt(min, max) {
@@ -129,7 +198,7 @@
     return key;
   }
 
-  function keyboardSetup(aFace) {
+  function setupKeyboard(aFace) {
     let left = keyboard(65),
       up = keyboard(87),
       right = keyboard(68),
